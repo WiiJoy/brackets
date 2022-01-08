c05f561142529afa44f9fb17cc8c00bbbe81eee9
WiiJoy MacOS <wiijoy@bk.ru>
2022-01-08 17:51:28 +0300

    feat: add function's body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  // const bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
+
+  let handledStr = str;
+
+  // let handledStr = '[]][[]'
+
+  let brackets = [];
+
+  bracketsConfig.forEach(bracket => {
+    brackets.push(bracket.join(''))
+  });
+
+  console.log(brackets);
+
+  for (let i = 0; i < brackets.length;) {
+    if (handledStr.includes(brackets[i])) {
+      handledStr = handledStr.replace(brackets[i], '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  return handledStr ? false : true;
+
 }
